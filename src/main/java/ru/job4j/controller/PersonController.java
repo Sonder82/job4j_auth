2e8a16bf6cc9b802f612ce1a9b902f9368613fe3
Sonder82 <Himrs82@gmail.com>
2023-08-22 13:19:49 +0500

    5. Обработка исключений и Spring REST [#504797]

--- src/main/java/ru/job4j/controller/PersonController.java
+++ src/main/java/ru/job4j/controller/PersonController.java
@@ -1,21 +1,34 @@
 package ru.job4j.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 import ru.job4j.model.Person;
 import ru.job4j.service.PersonService;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Optional;
 
 @RestController
 @RequestMapping("/person")
 @AllArgsConstructor
+@ControllerAdvice
 public class PersonController {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(PersonController.class);
+
     private final PersonService persons;
+
+    private final ObjectMapper objectMapper;
 
     @GetMapping("/all")
     public List<Person> findAll() {
@@ -26,13 +39,21 @@ public class PersonController {
     public ResponseEntity<Person> findById(@PathVariable int id) {
         var person = persons.findById(id);
         return new ResponseEntity<Person>(
-                person.orElse(new Person()),
-                person.isPresent() ? HttpStatus.OK : HttpStatus.NOT_FOUND
+                person.orElseThrow(() -> new ResponseStatusException(
+                        HttpStatus.NOT_FOUND, "Person is not found")), HttpStatus.OK
         );
     }
 
     @PostMapping("/sign-up")
     public ResponseEntity<Person> create(@RequestBody Person person) {
+        String login = person.getLogin();
+        String password = person.getPassword();
+        if (login == null || password == null) {
+            throw new NullPointerException("Login and password mustn't be empty");
+        }
+        if (login.length() < 6) {
+            throw new IllegalArgumentException("Invalid password. Password length must be more than 5 characters.");
+        }
         Optional<Person> optionalPerson = persons.save(person);
         return new ResponseEntity<Person>(
                 optionalPerson.orElse(new Person()),
@@ -41,11 +62,36 @@ public class PersonController {
 
     @PutMapping("/")
     public ResponseEntity<Void> update(@RequestBody Person person) {
+        String login = person.getLogin();
+        String password = person.getPassword();
+        if (login == null || password == null) {
+            throw new NullPointerException("Login and password mustn't be empty");
+        }
+        if (login.length() < 6) {
+            throw new IllegalArgumentException("Invalid password. Password length must be more than 5 characters.");
+        }
         return persons.update(person) ? ResponseEntity.ok().build() : ResponseEntity.notFound().build();
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> delete(@PathVariable int id) {
-        return persons.deleteById(id) ? ResponseEntity.ok().build() : ResponseEntity.internalServerError().build();
+        if (!persons.deleteById(id)) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Person is not found");
+        }
+        return ResponseEntity.ok().build();
+    }
+
+    @ExceptionHandler(value = {IllegalArgumentException.class, NullPointerException.class})
+    public void exceptionHandler(Exception exception, HttpServletRequest request, HttpServletResponse response)
+            throws IOException {
+        response.setStatus(HttpStatus.BAD_REQUEST.value());
+        response.setContentType("application/json");
+        response.getWriter().write(objectMapper.writeValueAsString(new HashMap<>() {
+            {
+                put("message", exception.getMessage());
+                put("type", exception.getClass());
+            }
+        }));
+        LOGGER.error(exception.getLocalizedMessage());
     }
 }
